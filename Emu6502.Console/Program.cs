0226d1a2c1eda4b9e71523956737018080fdf457
Greger Olofsson <greger.olofsson@gmail.com>
2023-08-29 01:55:08 +0200

    Added 6502 functional tests to verify implementation. (Failed)

--- Emu6502.Console/Program.cs
+++ Emu6502.Console/Program.cs
@@ -1,24 +1,45 @@
 ﻿using Emu6502;
+using Instr = Emu6502.Cpu.Instructions;
 
-var memory = new byte[0xffff];
-ushort mem = 0;
-memory[mem++] = Cpu.Instructions.LDA_Immediate;
-memory[mem++] = 0x01;
-memory[mem++] = Cpu.Instructions.CLC;
-memory[mem++] = Cpu.Instructions.ADC_Immediate;
-memory[mem++] = 0x01;
-memory[mem++] = Cpu.Instructions.STA_Absolute;
-memory[mem++] = 0x00;
-memory[mem++] = 0x02;
-memory[mem++] = Cpu.Instructions.JMP_Absolute;
-memory[mem++] = 0x02;
-memory[mem++] = 0x00;
+var memory = new byte[0x10000];
+byte[] fileBytes = File.ReadAllBytes("6502_functional_test.bin");
+//ushort mem = 0;
+//memory[mem++] = Instr.LDA_Immediate; //init
+//memory[mem++] = 0x40;
+//memory[mem++] = Instr.LDX_Immediate;
+//memory[mem++] = 0x5B;
+//memory[mem++] = Instr.STA_Absolute;
+//memory[mem++] = 0x00;
+//memory[mem++] = 0x02;
+//
+//memory[mem++] = Instr.INC_Absolute; //inc
+//memory[mem++] = 0x00;
+//memory[mem++] = 0x02;
+//memory[mem++] = Instr.CPX_Absolute;
+//memory[mem++] = 0x00;
+//memory[mem++] = 0x02;
+//memory[mem++] = Instr.BEQ;//reset if x reached
+//memory[mem++] = 0x04;
+//memory[mem++] = 0x00;
+//
+//memory[mem++] = Instr.JMP_Absolute;//cont inc
+//memory[mem++] = 0x07;
+//memory[mem++] = 0x00;
 
-Cpu cpu = new(memory);
+Cpu cpu = new(fileBytes);
 cpu.Reset();
+cpu.Registers.PC = 0x0400;
 
-for(int i = 0; i < 1000; i++)
+var trapMonitor = new ushort[6];
+ushort lastPC = 0xffff;
+trapMonitor[0] = 0xff;
+var tIndex = 0;
+
+//for(int i = 0; i < 1000; i++)
+while (true)
 {
+    cpu.Execute(1);
+
     Console.SetCursorPosition(0, 0);
     Console.WriteLine(
         $"A: 0x{cpu.Registers.A:X2}   " +
@@ -33,16 +54,31 @@ for(int i = 0; i < 1000; i++)
 
     Console.SetCursorPosition(0, 5);
 
-    cpu.Execute(1);
-    switch (memory[0x0200])
+    
+    if(lastPC != cpu.Registers.PC)
     {
-        case > 0x40 and < 0x5B:
-            var pos = Console.GetCursorPosition();
-            Console.SetCursorPosition(2, 2);
-            Console.Write((char)memory[0x0200]);
-            Console.SetCursorPosition(pos.Left, pos.Top);
-            break;
-    };
+        lastPC = cpu.Registers.PC;
+        trapMonitor[tIndex++] = cpu.Registers.PC;
+        tIndex %= 6;
+    }
+    if (trapMonitor[0] == trapMonitor[3]
+        && trapMonitor[1] == trapMonitor[4]
+        && trapMonitor[2] == trapMonitor[5])
+    {
+        Console.WriteLine($"Trapped at 0x{trapMonitor[1]:X4}");
+        break;
+    }
+    //switch (memory[0x0200])
+    //{
+    //    case > 0x40 and < 0x5B:
+    //        var pos = Console.GetCursorPosition();
+    //        Console.SetCursorPosition(2, 2);
+    //        Console.Write((char)memory[0x0200]);
+    //        Console.SetCursorPosition(pos.Left, pos.Top);
+    //        break;
+    //};
+
+    await Task.Delay(1);
 }
 
 string ToNamedSr(byte b)
